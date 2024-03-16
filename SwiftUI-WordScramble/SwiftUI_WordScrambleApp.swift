b5e49e98386f5f0262262670069a5ccd89874eb4
Jimmy <myname90196@gmail.com>
2024-03-16 12:04:16 +0800

    Initial Commit

---
+++ SwiftUI-WordScramble/SwiftUI_WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WordScrambleApp.swift
+//  SwiftUI-WordScramble
+//
+//  Created by JimmyChao on 2024/3/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
